9fb530dc1b85022bf8ed920cfcab97de5586f629
Vert Simon <Stopmotion45b@gmail.com>
2022-06-25 11:58:57 -0400

    Update testing.c

--- testing.c
+++ testing.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+//The last arg is a return value pointer
+//as courtesy, external assembly functions will exhibit no C-Style return convention
 extern int AssemblyFunc(int num1, int num2, int *returnVal);
 
 int main() {

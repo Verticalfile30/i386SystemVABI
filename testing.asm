05669123ab8d46369c52e9b334c715241c82c763
Vert Simon <Stopmotion45b@gmail.com>
2022-06-25 11:57:36 -0400

    Update testing.asm

--- testing.asm
+++ testing.asm
@@ -5,16 +5,19 @@ global AssemblyFunc
 
 AssemblyFunc:
 	
+	;function prologue
 	push ebp
 	mov ebp, esp
 	pusha
 	
+	;Use a pointer for the return value.
 	mov eax, [ebp + 8]
 	mov ebx, [ebp + 12]
 	add eax, ebx
 	mov ecx, [ebp + 16]
 	mov [ecx], eax
 	
+	;function epilogue
 	popa
 	;mov esp, ebp
 	pop ebp
